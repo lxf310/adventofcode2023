a358b08f7ffaf93d89c8fb5f4d8c71702d35b254
Xiaofeng Lei <Xiaofeng.Lei@morganstanley.com>
2023-12-22 01:43:11 +0000

    day twentyone part 1

--- Event2023/Event2023/Program.cs
+++ Event2023/Event2023/Program.cs
@@ -1,7 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Event2023;
-using Event2023.Puzzles.DayTwenty;
 
-var solver = new DayTwentyPart2();
-Console.WriteLine(solver.Total(Helper.ReadInputs("Inputs\\20.txt")));
+var solver = new DayTwentyOnePart2();
+Console.WriteLine(solver.Total(Helper.ReadInputs("Inputs\\21.txt"), 6));
+//Console.WriteLine(solver.Total(Helper.ReadInputs("Inputs\\21.txt"), 10));
+//Console.WriteLine(solver.Total(Helper.ReadInputs("Inputs\\21.txt"), 50));
+//Console.WriteLine(solver.Total(Helper.ReadInputs("Inputs\\21.txt"), 100));
+//Console.WriteLine(solver.Total(Helper.ReadInputs("Inputs\\21.txt"), 500));
+//Console.WriteLine(solver.Total(Helper.ReadInputs("Inputs\\21.txt"), 1000));
+//Console.WriteLine(solver.Total(Helper.ReadInputs("Inputs\\21.txt"), 5000));
---
+++ Event2023/Event2023/Puzzles/DayTwentyOne/DayTwentyOnePart1.cs
@@ -0,0 +1,61 @@
+﻿namespace Event2023.Puzzles.DayTwentyOne
+{
+    public class DayTwentyOnePart1
+    {
+        private List<Tuple<int, int>> _directions = new List<Tuple<int, int>>
+            {
+                new Tuple<int, int>(-1, 0),
+                new Tuple<int, int>(0, 1),
+                new Tuple<int, int>(1, 0),
+                new Tuple<int, int>(0, -1),
+            };
+
+        public long Total(List<string> inputs, int cnt)
+        {
+            var memo = new HashSet<Tuple<int, int>>();
+            var m = inputs.Count;
+            var n = inputs[0].Length;
+
+            Tuple<int, int> start = null;
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (inputs[i][j] == 'S')
+                    {
+                        start = new Tuple<int, int>(i, j);
+                        break;
+                    }
+                }
+
+                if (start != null)
+                {
+                    break;
+                }
+            }
+
+            memo.Add(start);
+
+            while (cnt-- > 0 && memo.Any())
+            {
+                var temp = new HashSet<Tuple<int, int>>();
+                foreach (var pos in memo)
+                {
+                    foreach (var direction in _directions)
+                    {
+                        var ii = pos.Item1 + direction.Item1;
+                        var jj = pos.Item2 + direction.Item2;
+                        if (ii >= 0 && ii < m && jj >= 0 && jj < n && inputs[ii][jj] != '#')
+                        {
+                            temp.Add(new Tuple<int, int>(ii, jj));
+                        }
+                    }
+                }
+
+                memo = temp;
+            }
+
+            return memo.LongCount();
+        }
+    }
+}
